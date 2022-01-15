6e635f62e57c21e7f3bba45522672df24459fb73
Demaro Jones <demarojones@Demaros-iMac.local>
2022-01-15 15:57:06 -0500

    Added React Google Map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from "@mui/material";
+import { LocationOnOutlined } from '@mui/icons-material';
+import { Rating } from "@mui/material";
+
+import useMapStyles from "./mapStyles";
 
 const Map = () => {
+    const classes = useMapStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+    const coordinates = {lat: 0, lng: 0}
     return (
-        <div>
-            MAP WILL DISPLAY HERE
+        <div className={classes.mapContainer}>
+            <GoogleMapReact bootstrapURLKeys={{key: ''}} 
+                defaultCenter={coordinates} 
+                center={coordinates} 
+                defaultZoom={14} 
+                margin={[50,50,50,50]}
+                options={''}
+                onChange={()=> ''}
+                onChildClick={()=> ''}>
+
+            </GoogleMapReact>
         </div>
     )
 }
